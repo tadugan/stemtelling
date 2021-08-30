de81eae4b45177f46dafa6d3e7c1c9478459249c
Sagethh <tate.feige@gmail.com>
2021-08-30 15:59:37 -0500

    code checker

--- src/components/ForgotPasswordPage/ForgotPasswordPage.jsx
+++ src/components/ForgotPasswordPage/ForgotPasswordPage.jsx
@@ -4,23 +4,49 @@ import {useSelector} from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import cryptoRandomString from 'crypto-random-string';
 import axios from 'axios';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+
+const useStyles = makeStyles((theme) => ({
+   modal: {
+     display: 'flex',
+     alignItems: 'center',
+     justifyContent: 'center',
+   },
+   paper: {
+     backgroundColor: theme.palette.background.paper,
+     border: '2px solid #000',
+     boxShadow: theme.shadows[5],
+     padding: theme.spacing(2, 4, 3),
+   },
+ }));
+
+ 
 
 function ForgotPasswordPage() {
    const history = useHistory();
+   const [resetCode, setResetCode] = useState('');
    const [inputCode, setInputCode] = useState('');
    const [email, setEmail] = useState('');
    const errors = useSelector(store => store.errors);
    const dispatch = useDispatch();
-
+   const classes = useStyles();
+   const [open, setOpen] = React.useState(false);
+   const handleOpen = () => {setOpen(true)};
+   const handleClose = () => {setOpen(false)};
 
    const resetPassword = () => { // main handler for password resetting, collects data from email input and dispatches it
       event.preventDefault();
+      setResetCode('');
       if (email == null || email == undefined || email == '' || email == ' ') {
          alert('Invalid Email');
          setEmail('');  
          return false;
       };
-      const newCode = cryptoRandomString({length: 32, type: 'base64'});
+      let newCode = cryptoRandomString({length: 32, type: 'base64'});
+      setResetCode(newCode);
       dispatch({
          type: 'FORGOT_PASSWORD',
          payload: {
@@ -30,8 +56,22 @@ function ForgotPasswordPage() {
       });
       setEmail('');
       console.log("reset code is:", newCode);
+      handleOpen();
    };
    // end resetPassword handler
+
+   const checkCode = () => {
+      console.log(resetCode);
+      console.log(inputCode);
+      if (resetCode != inputCode) {
+         alert('Invalid code');
+         setInputCode('');
+         return false;
+      }
+      if (resetCode == inputCode) {
+         alert.log("Now proceeding to reset password!");
+      }
+   }
 
    
   return (
@@ -49,20 +89,38 @@ function ForgotPasswordPage() {
         </label>
       </div>
       <div>
-        <label htmlFor="code">
-          Code:
-          <input
-            type="text"
-            name="code"
-            required
-            value={inputCode}
-            onChange={(event) => setInputCode(event.target.value)}
-          />
-        </label>
-      </div>
-      <div>
          <button className="btn" onClick={resetPassword}>Reset Password</button>
       </div>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>
+            <h2 id="transition-modal-title">Enter Confirmation Code</h2>
+            <label htmlFor="code">
+               Code:
+               <input
+                  type="text"
+                  name="code"
+                  required
+                  value={inputCode}
+                  onChange={(event) => setInputCode(event.target.value)}
+               />
+            </label>
+            <br />
+            <button className="btn" onClick={checkCode}>Confirm</button>
+          </div>
+        </Fade>
+      </Modal>
    </div>
   );
 };
