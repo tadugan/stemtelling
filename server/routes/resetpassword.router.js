35fe86add6d9c45fb3716be9518181bdc5650b9a
Sagethh <tate.feige@gmail.com>
2021-08-31 15:31:57 -0500

    added notes to resetpassword router

--- server/routes/resetpassword.router.js
+++ server/routes/resetpassword.router.js
@@ -2,9 +2,10 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 var nodemailer = require('nodemailer');
+require('dotenv').config();
 const { URLSearchParams } = require('url');
 
-const transporter = nodemailer.createTransport({
+const transporter = nodemailer.createTransport({ // nodemailer handler, auth should be stored as a local .env variable and input in auth to avoid sensitive data leaking
    service: 'Gmail',
    auth: {
        user: 'Stemtelltest@gmail.com',
@@ -13,7 +14,7 @@ const transporter = nodemailer.createTransport({
 });
 const websiteURL = new URL ("http://localhost:3000/resetpassword/"); // origin point for resetting a password, will later be appended with a confirmation code
 
-router.post('/email', (req, res) => { // handler for resetting a user's password
+router.post('/sendresetemail', (req, res) => { // handler for sending out a password reset email to the user, as well as creating a temporary database placement for them
    const userEmail = req.body.email.toLowerCase();
    const getUserIDQuery = `SELECT * FROM "user" WHERE "email" = $1`;
 
@@ -23,46 +24,39 @@ router.post('/email', (req, res) => { // handler for resetting a user's password
       const insertUserInfoQuery = `INSERT INTO "reset_password" (id, email)
                      VALUES ($1, $2) RETURNING id`;
       pool.query(insertUserInfoQuery, [userID, userEmail]) // start of second query
-      .then(results => {
+      .then(() => {
          const getUUIDQuery = `SELECT "uuid" FROM "reset_password" WHERE "id" = $1 AND "email" = $2`;
          pool.query(getUUIDQuery, [userID, userEmail]) // start of third query
-         .then((results) => {
+         .then(results => {
             const resetPasswordUUID = results.rows[0].uuid; 
-            websiteURL.searchParams.append("confirmation", resetPasswordUUID);
-            console.log(websiteURL.href);
-
-
-            const resetEmail = {
+            websiteURL.searchParams.append("confirmation", resetPasswordUUID); // append UUID to url params for confirmation 
+            const resetEmail = { // nodemailer handler, this is what the email will be
                from: 'Stemtelltest@gmail.com',
                to: `${userEmail}`,
                subject: 'STEMTelling Password Reset Request',
-               text: `
-               ${websiteURL.href}
-               
-               
-               `};
-            
-            transporter.sendMail(resetEmail, (error, info) => {
+               text: `${websiteURL.href}`
+            };
+            transporter.sendMail(resetEmail, (error, info) => { // sends the email via nodemailer
                if (error) {
-                  return console.log(error);
-               }
-               res.status(200).send({message: "Mail send", message_id: info.messageId});
+                  return console.log(error); // catch any nodemailer errors
+               };
+               res.status(200).send({message: "Mail send", message_id: info.messageId}); // success
             });
+            res.sendStatus(200); // send pack a positive status code for our third query
          })
-         .catch((error) => { // end of third query
-            console.log(error);  
+         .catch(error => { // end of third query
+            console.log("Error in third query in password reset:", error); // catch any errors in our third query
             res.sendStatus(401);
          });
       })
       .catch(error => { // end of second query   
-         console.log(error);
+         console.log("Error in second query in password reset:", error); // catch any errors in our second query
          res.sendStatus(401);
       });
    })
    .catch(error => { // end of first query
-      console.log(error);
-      console.log("Email does not exist in database");
-      res.sendStatus(404);
+      console.log("Error in first query in password reset:", error); // catch any errors in our first query
+      res.sendStatus(401);
    });
 });
 
