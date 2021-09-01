1ea78d0b1589e18c74b23be8f511911033d4c47b
Sagethh <tate.feige@gmail.com>
2021-09-01 14:31:39 -0500

    working on user profile page

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,18 +1,43 @@
 import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector} from 'react-redux';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
 
 function UserPage() {
-  // this component doesn't do much to start, just renders some user reducer info to the DOM
-  const user = useSelector((store) => store.user);
-  return (
-    <div className="container">
-      <h2>Welcome, {user.name}!</h2> 
-      <p>Your ID is: {user.id}</p>
-      <LogOutButton className="btn" />
+   const classes = useStyles();
+   const user = useSelector((store) => store.user);
+   return (
+      <div className={classes.root}>
+      <Grid container   >
+        <Grid item xs={12} sm={3}>
+          <Paper className={classes.paper}>
+            <h2>Welcome, {user.name}!</h2> 
+            <p>Your ID is: {user.id}</p>
+           <LogOutButton className="btn" />
+          
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={9}>
+          <Paper className={classes.paper}>User Profile Feed</Paper>
+        </Grid>
+      </Grid>
     </div>
   );
 }
 
-// this allows us to use <App /> in index.js
+
 export default UserPage;
