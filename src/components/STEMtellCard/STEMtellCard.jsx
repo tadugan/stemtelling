be9fa9d10cb6357021348c12a60c0057b844aad2
Meghan Cleland <mplscleland@gmail.com>
2021-08-31 19:12:16 -0500

    added stemtell GET route, reducer, and saga

--- src/components/STEMtellCard/STEMtellCard.jsx
+++ src/components/STEMtellCard/STEMtellCard.jsx
@@ -1,29 +1,47 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Avatar, Card, Grid } from "@material-ui/core";
 // import { makeStyles } from "@material-ui/core";
 import "./STEMtellCard.css";
+import { useDispatch, useSelector } from "react-redux";
+
 
 function StemtellCard() {
+
+  useEffect(() => {
+    dispatch({ type: "FETCH_STEMTELLS"});
+  }, []);
+
+  const dispatch = useDispatch();
+  const stemtells = useSelector((store) => store.stemtells);
+
+  console.log(stemtells, "THIS IS THE STEMTELL STORE*****");
+
+
 
 
   return (
     <Grid>
-      <Grid item>
+      {stemtells.map((stemtell) => {
+        return (
+      <Grid item >
         <Card className="StemCard">
           <Avatar className="Avatar" />
-          <section className="UserName">User's Name</section>
+          <section className="UserName">{stemtell.username}</section>
           <div className="UserName" id="userClass">
-            User Class
+            {stemtell.class_name}
           </div>
-          <h3> STEMTell Title</h3>
-          <img src="https://upload.wikimedia.org/wikipedia/commons/5/51/High_School_Earth_Science_Cover.jpg" />
-          <section id="cardReactions">emojis go here</section>
+          <h3> {stemtell.title}</h3>
+          <img src={stemtell.media_url} />
+          <section id="cardReactions">{stemtell.reaction_name}</section>
 
-          <section id="stemDescription">This is where descriptions go</section>
+          <section id="stemDescription">{stemtell.body_text}</section>
         </Card>
       </Grid>
+        );
+        })};
     </Grid>
-  );
-}
+    
+)
+};
 
 export default StemtellCard;
