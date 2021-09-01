673540ad8331574d8a5c5afb6c2df74ad919fb31
Meghan Cleland <mplscleland@gmail.com>
2021-09-01 15:36:03 -0500

    connected class card to back end with saga and reducer

--- server/routes/class.router.js
+++ server/routes/class.router.js
@@ -7,6 +7,18 @@ const router = express.Router();
  */
 router.get('/', (req, res) => {
   // GET route code here
+  const query = `SELECT * FROM "class"
+  JOIN "user_class" ON "user_class".class_id= "class".id
+  WHERE "user_class".user_id = 3;`
+  pool
+  .query(query)
+  .then((result) => {
+    res.send(result.rows);
+  })
+  .catch((err) => {
+    console.log("Error getting teacher's classes", err);
+    res.sendStatus(500);
+  })
 });
 
 /**
--- src/components/STEMtellCard/STEMtellCard.jsx
+++ src/components/STEMtellCard/STEMtellCard.jsx
@@ -43,8 +43,8 @@ function StemtellCard() {
           <section id="stemDescription">{stemtell.body_text}</section>
         </Card>
       </Grid>
-        );
-        })};
+        )
+        })}
     </Grid>
     </Container>
     
