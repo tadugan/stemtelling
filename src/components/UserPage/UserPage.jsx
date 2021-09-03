27d5986570b2762d20a641dc06750492c4eccdb0
Sagethh <tate.feige@gmail.com>
2021-09-03 18:10:24 -0500

    working on edit stemtell modal

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,12 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import LogOutButton from '../LogOutButton/LogOutButton';
 import { useDispatch, useSelector } from "react-redux";
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
-import Avatar from '@material-ui/core/Avatar';
 import { useHistory } from 'react-router';
+import { Button, FormControl, Grid, InputLabel, MenuItem, Select, TextField, Avatar, Card, Paper, Modal, Backdrop, Fade } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -19,12 +16,31 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const modalStyles = makeStyles((theme) => ({
+   modal: {
+     display: 'flex',
+     alignItems: 'center',
+     justifyContent: 'center',
+   },
+   paper: {
+     backgroundColor: theme.palette.background.paper,
+     border: '2px solid #000',
+     boxShadow: theme.shadows[5],
+     padding: theme.spacing(2, 4, 3),
+   },
+ }));
+
 function UserPage() {
    const classes = useStyles();
+   const modalClasses = modalStyles();
    const user = useSelector((store) => store.user);
    const dispatch = useDispatch();
    const history = useHistory();
    const stemtells = useSelector((store) => store.stemtells);
+   const [open, setOpen] = React.useState(false);
+   const [editStemtellID, setEditStemtellID] = useState('');
+   const handleOpen = (stemID) => {setOpen(true); setEditStemtellID(stemID); console.log(stemID)};
+   const handleClose = (stemID) => {setOpen(false)};
 
    useEffect(() => {
       dispatch({ type: "FETCH_USER_STEMTELLS", payload: user.id });
@@ -61,7 +77,7 @@ function UserPage() {
                         <img src={stemtell.media_url} />
                         <section id="cardReactions">{stemtell.reaction_name}</section>
                         <section id="stemDescription">{stemtell.body_text}</section>
-                        <button value={stemtell.id} className="btn" onClick={event => {editStemtell(event.target.value)}}>
+                        <button value={stemtell.id} className="btn" onClick={(event) => {handleOpen(event.target.value)}}>
                            Edit
                         </button>
                      </Card>
@@ -70,6 +86,27 @@ function UserPage() {
             })}
          </Grid>
         </Grid>
+        <Modal
+         aria-labelledby="email confirmation modal"
+         aria-describedby="email confirmation modal"
+         className={modalClasses.modal}
+         open={open}
+         onClose={handleClose}
+         closeAfterTransition
+         BackdropComponent={Backdrop}
+         BackdropProps={{
+            timeout: 500,
+         }}
+        >
+         <Fade in={open}>
+            <div className={modalClasses.paper}>
+               <p id="email-confirmation-modal-description">
+                  An email has been sent. If you don't see it, check for previous emails, or check your spam/junk folder.
+               </p>
+               <button className="btn" onClick={() => {history.push('/')}}>Return Home</button>
+            </div>
+         </Fade>
+      </Modal>
       </Grid>
     </div>
   );
