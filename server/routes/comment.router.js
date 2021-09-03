ec98a2fe7c614bfd41da11b5bc81af02ec5715e6
Kash <kserenitym@gmail.com>
2021-09-03 09:56:32 -0500

    pulled from master branch

--- server/routes/comment.router.js
+++ server/routes/comment.router.js
@@ -6,24 +6,25 @@ const rejectUnauthenticated = require('../modules/authentication-middleware').re
 /**
  * GET route template
  */
-// router.get('/', rejectUnauthenticated, (req, res) => {
-//   const query= `SELECT "user".name AS username, "stemtell".id, "user".profile_picture_url, "comment".comment, "comment".date_published, "comment".id, "user".authority 
-//   FROM "comment"
-//   JOIN "user" ON "comment".user_id = "user".id
-//   JOIN "stemtell" ON "stemtell".id = "comment".stemtell_id
-//   WHERE  "user".authority = 'student'  
-//   ORDER BY "date_published" desc;`;
-//   pool
-//     .query(query)
-//     .then((result) => {
-//       console.log('successful GETTING comments', result.rows);
-//       res.send(result.rows);
-//     })
-//     .catch((err) => {
-//       console.log('error GETTING comments', err);
-//     });
+router.get('/', rejectUnauthenticated, (req, res) => {
+  const query= `select "user".name, "user".profile_picture_url , "comment".comment , "comment".date_published ,"comment".stemtell_id, "comment".teacher_feedback ,"comment".user_id, "stemtell".id, "comment".id
+  from "comment"
+  join "user" on "user".id = "comment".user_id 
+  join "stemtell" on "stemtell".id = "comment".stemtell_id 
+  where "comment".stemtell_id = 1 
+  and "comment".teacher_feedback = false 
+  order by "comment".date_published desc;`;
+  pool
+    .query(query)
+    .then((result) => {
+      console.log('successful GETTING comments', result.rows);
+      res.send(result.rows);
+    })
+    .catch((err) => {
+      console.log('error GETTING comments', err);
+    });
      
-// });
+});
 
 router.get('/feedback', rejectUnauthenticated, (req, res) => {
   // GET route code here
