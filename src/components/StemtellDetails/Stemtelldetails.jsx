81045e4f233a90f5e64997cc9374f574818d4d79
Kash <kserenitym@gmail.com>
2021-09-06 17:23:47 -0500

    reformatted code

--- src/components/StemtellDetails/Stemtelldetails.jsx
+++ src/components/StemtellDetails/Stemtelldetails.jsx
@@ -14,31 +14,29 @@ import Comment from "../Comment/Comment";
 import BackBtn from "../BackBtn/BackBtn";
 import { Details } from "@material-ui/icons";
 
-
 function StemtellDetails() {
-
-    const params = useParams();
-    const stemtellId= params.id;
-    console.log('This is the stemtell id:', stemtellId);
+  const params = useParams();
+  const stemtellId = params.id;
+  console.log("This is the stemtell id:", stemtellId);
   const dispatch = useDispatch();
-//   const getSearchQueryByFullURL = (url) => {return url.split('/')};
+  //   const getSearchQueryByFullURL = (url) => {return url.split('/')};
 
   useEffect(() => {
-    dispatch({ type: "FETCH_STEMTELL_DETAILS", payload: stemtellId});
+    dispatch({ type: "FETCH_STEMTELL_DETAILS", payload: stemtellId });
   }, []);
 
   const stemtell = useSelector((store) => store.stemtellDetails);
-console.log('STEMDETAILS STORE:', stemtell)
+  console.log("STEMDETAILS STORE:", stemtell);
 
   const onUserProfile = (author_id) => {
     //  TODO: If this is working, remove console.log()
-     console.log("Clicked profile with author_id:", author_id);
-     history.push(`/profile/${author_id}`);
-   }
+    console.log("Clicked profile with author_id:", author_id);
+    history.push(`/profile/${author_id}`);
+  };
   return (
     <>
       <BackBtn />
-<center>
+      <center>
         <Grid item key={stemtell.id}>
           <Card className="StemCard">
             <h6 id="stemDate">{stemtell.date_published}</h6>
@@ -53,11 +51,7 @@ console.log('STEMDETAILS STORE:', stemtell)
               {stemtell.class_name}
             </div>
 
-            <h3
-              id="stemTitle"
-            >
-              {stemtell.title}
-            </h3>
+            <h3 id="stemTitle">{stemtell.title}</h3>
 
             <img src={stemtell.media_url} />
             <section id="cardReactions">{stemtell.reaction_name}</section>
@@ -65,7 +59,7 @@ console.log('STEMDETAILS STORE:', stemtell)
             <section id="stemDescription">{stemtell.body_text}</section>
           </Card>
         </Grid>
-</center>
+      </center>
       <Comment />
     </>
   );
