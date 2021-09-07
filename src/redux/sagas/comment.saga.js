b7e61e306dfcf272edf335d1d0b7853ce3b78b22
Tim Dugan <tadugan@me.com>
2021-09-07 13:14:50 -0500

    Added sendStatus to stemtell router. This fixed the comment refresh issue.

--- src/redux/sagas/comment.saga.js
+++ src/redux/sagas/comment.saga.js
@@ -17,15 +17,10 @@ function* getComments() {
     }
 }
 
-//TODO: work on posting a comment to a stemtell
 function* addComment(action){
     try{
-        console.log('In addComment. action.payload:', action.payload); // TODO:
-        yield call(axios.post, '/api/comment', action.payload);
-        // yield put({type: 'GET_STEMTELL_COMMENTS', payload: action.payload.stemtell_id});
-        const response = yield axios.get(`/api/comment/stemcomments/${action.payload.stemtell_id}`);
-        console.log('this is after getting stemtell comments. response:', response); // TODO:
-        yield put({ type: 'SET_STEMTELL_COMMENTS', payload: response.data});
+        yield axios.post('/api/comment', action.payload);
+        yield put({ type: 'GET_STEMTELL_COMMENTS', payload: action.payload.stemtell_id});
     } catch(error){
         console.log('unable to add comment', error);
     }
@@ -33,8 +28,6 @@ function* addComment(action){
 
 function* getStemComments(action) {
     try {
-        console.log('In getStemComments'); // TODO:
-        console.log('Action Payload for getStemComments:', action.payload); // TODO:
         const response = yield axios.get(`/api/comment/stemcomments/${action.payload}`);
         yield put({ type: 'SET_STEMTELL_COMMENTS', payload: response.data});
     } catch (error) {
