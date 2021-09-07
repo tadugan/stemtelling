cc0e1da6705136e31a9e66793423a1b2fb986e00
Sagethh <tate.feige@gmail.com>
2021-09-07 14:44:41 -0500

    fixed mobile view margain

--- src/components/STEMtellCard/STEMtellCard.jsx
+++ src/components/STEMtellCard/STEMtellCard.jsx
@@ -6,60 +6,53 @@ import "./STEMtellCard.css";
 
 
 function StemtellCard() {
-  const history = useHistory();
-  const dispatch = useDispatch();
-  const stemtells = useSelector((store) => store.stemtells);
+   const history = useHistory();
+   const dispatch = useDispatch();
+   const stemtells = useSelector((store) => store.stemtells);
 
-  useEffect(() => {
-    dispatch({ type: "FETCH_CLASSES"})
-    dispatch({ type: "FETCH_STEMTELLS"}); //need to use the value from FETCH CLASSES
-  }, []);
+   useEffect(() => {
+      dispatch({ type: "FETCH_CLASSES"})
+      dispatch({ type: "FETCH_STEMTELLS"});
+   }, []);
 
-  const onUserProfile = (author_id) => {
-    history.push(`/profile/${author_id}`);
-  }
+   const onUserProfile = (author_id) => {
+      history.push(`/profile/${author_id}`);
+   };
 
-  const toStemtellDetail = (stem_id) => {
-    history.push(`stemtell/${stem_id}`);
-  }
+   const toStemtellDetail = (stem_id) => {
+      history.push(`stemtell/${stem_id}`);
+   };
 
-  const unixTimestamp = (timestamp) => {
-   const dateObject = new Date((timestamp * 1000));
+   const unixTimestamp = (timestamp) => {
+      const dateObject = new Date((timestamp * 1000));
+      return (
+         dateObject.toLocaleString([], {year: 'numeric', month: 'numeric', day: 'numeric', hour: '2-digit', minute: '2-digit'})
+      );
+   };
+
    return (
-      dateObject.toLocaleString([], {year: 'numeric', month: 'numeric', day: 'numeric', hour: '2-digit', minute: '2-digit'})
+      <>
+         {stemtells.map((stemtell) => {
+            return (
+               <Grid item key={stemtell.id} aria-labelledby="homepage stemtell item">
+                  <Card className="StemCard">
+                     <h6 id="stemDate">{unixTimestamp(stemtell.unix)}</h6>
+                     <Avatar className="Avatar"src={stemtell.profile_picture_url} />
+                     <section className="UserName" onClick={() => onUserProfile(stemtell.author_id)}>{stemtell.username}</section>
+                     <div className="UserName" id="userClass">
+                        {stemtell.class_name}
+                     </div>
+                     <h3 id="stemTitle" onClick={() => toStemtellDetail(stemtell.stem_id)}> {stemtell.title}</h3>
+                     <img id="stemtellImage" src={stemtell.media_url} onClick={() => toStemtellDetail(stemtell.stem_id)}/>
+                     <section id="cardReactions">{stemtell.reaction_name}</section>
+                     <section id="stemDescription" onClick={() => toStemtellDetail(stemtell.stem_id)}>{stemtell.body_text}</section>
+                  </Card>
+               </Grid>
+            )
+         })}
+      </>
    );
-  }
-
-  return (
-    <Grid
-      item
-      container
-      aria-labelledby="homepage stemtell container"
-    >
-      {stemtells.map((stemtell) => {
-        return (
-      <Grid item key={stemtell.id} aria-labelledby="homepage stemtell item">
-        <Card className="StemCard">
-        <h6 id="stemDate">{unixTimestamp(stemtell.unix)}</h6>
-          <Avatar className="Avatar"
-          src={stemtell.profile_picture_url} />
-          <section className="UserName" onClick={() => onUserProfile(stemtell.author_id)}>{stemtell.username}</section>
-          <div className="UserName" id="userClass">
-            {stemtell.class_name}
-          </div>
-
-          <h3 id="stemTitle" onClick={() => toStemtellDetail(stemtell.stem_id)}> {stemtell.title}</h3>
-
-          <img id="stemtellImage" src={stemtell.media_url} onClick={() => toStemtellDetail(stemtell.stem_id)}/>
-          <section id="cardReactions">{stemtell.reaction_name}</section>
-
-          <section id="stemDescription" onClick={() => toStemtellDetail(stemtell.stem_id)}>{stemtell.body_text}</section>
-        </Card>
-      </Grid>
-        )
-        })}
-    </Grid>
-)
 };
+
 
 export default StemtellCard;
--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -7,29 +7,30 @@ import EditSTEMtell from "../EditSTEMtell/EditSTEMtell";
 import './UserPage.css';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
+   root: {
+      flexGrow: 1,
+   },
+   paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+   },
 }));
 
 const modalStyles = makeStyles((theme) => ({
    modal: {
-     display: 'flex',
-     alignItems: 'center',
-     justifyContent: 'center',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
    },
    paper: {
-     backgroundColor: theme.palette.background.paper,
-     border: '2px solid #000',
-     boxShadow: theme.shadows[5],
-     padding: theme.spacing(2, 4, 3),
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
    },
  }));
+
 
 function UserPage() {
    const classes = useStyles();
@@ -50,62 +51,67 @@ function UserPage() {
       dispatch({ type: "FETCH_USER_STEMTELLS", payload: user.id });
     }, []);
 
-
    return (
       <div className={classes.root}>
-      <Grid container>
-        <Grid item xs={12} sm={3}> 
-          <Paper className={classes.paper}>
-            <img src={user.profile_picture_url}></img>
-            <h2>{user.name}</h2> 
-            <LogOutButton className="btn" />
-          </Paper>
-        </Grid>
-        <Grid item xs={12} sm={9}>
-          <Paper className={classes.paper}>Your STEMtells</Paper>
-          <Grid container>
-            {stemtells.map((stemtell) => {
-               return (
-                  <Grid item key={stemtell.id}>
-                     <Card className="StemCard">
-                        <Avatar className="Avatar" />
-                        <section className="UserName">{stemtell.username}</section>
-                        <div className="UserName" id="userClass">
-                           {stemtell.class_name}
-                        </div>
-                        <h3>{stemtell.title}</h3>
-                        <img id="stemtellImage" src={stemtell.media_url} />
-                        <section id="cardReactions">{stemtell.reaction_name}</section>
-                        <section id="userStemtellDescription">{stemtell.body_text}</section>
-                        <button className="btn" onClick={() => {handleOpen(stemtell)}}>
-                           Edit
-                        </button>
-                     </Card>
-                  </Grid>
-               )
-            })}
+         <Grid container direction="row"
+               justifyContent="center"
+               alignItems="flex-start"
+         >
+            <Grid item xs={12} sm={3}> 
+               <Paper className={classes.paper}>
+                  <img src={user.profile_picture_url}></img>
+                  <h2>{user.name}</h2> 
+                  <LogOutButton className="btn" />
+               </Paper>
+            </Grid>
+            <Grid item xs={12} sm={9}>
+               <Paper className={classes.paper}>Your STEMtells</Paper>
+               <Grid container direction="row"
+                     justifyContent="center"
+                     alignItems="flex-start"
+               >
+                  {stemtells.map((stemtell) => {
+                     return (
+                        <Grid item key={stemtell.id}>
+                           <Card className="StemCard">
+                              <Avatar className="Avatar" />
+                              <section className="UserName">{stemtell.username}</section>
+                              <div className="UserName" id="userClass">
+                                 {stemtell.class_name}
+                              </div>
+                              <h3>{stemtell.title}</h3>
+                              <img id="stemtellImage" src={stemtell.media_url} />
+                              <section id="cardReactions">{stemtell.reaction_name}</section>
+                              <section id="userStemtellDescription">{stemtell.body_text}</section>
+                              <button className="btn" onClick={() => {handleOpen(stemtell)}}>
+                                 Edit
+                              </button>
+                           </Card>
+                        </Grid>
+                     )
+                  })}
+               </Grid>
+            </Grid>
+            <Modal
+               aria-labelledby="email confirmation modal"
+               aria-describedby="email confirmation modal"
+               className={modalClasses.modal}
+               open={open}
+               onClose={handleClose}
+               closeAfterTransition
+               BackdropComponent={Backdrop}
+               BackdropProps={{
+                  timeout: 500,
+               }}
+            >
+               <Fade in={open}>
+                  <EditSTEMtell stemtell={stemtellData} key={stemtellData.id}/>
+               </Fade>
+            </Modal>
          </Grid>
-        </Grid>
-        <Modal
-         aria-labelledby="email confirmation modal"
-         aria-describedby="email confirmation modal"
-         className={modalClasses.modal}
-         open={open}
-         onClose={handleClose}
-         closeAfterTransition
-         BackdropComponent={Backdrop}
-         BackdropProps={{
-            timeout: 500,
-         }}
-        >
-         <Fade in={open}>
-            <EditSTEMtell stemtell={stemtellData} key={stemtellData.id}/>
-         </Fade>
-      </Modal>
-      </Grid>
-    </div>
-  );
-}
+      </div>
+   );
+};
 
 
 export default UserPage;
