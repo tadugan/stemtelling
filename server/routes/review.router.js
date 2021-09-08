cfdf9426eb4f2bc82657813785f90cc8567d263c
Tim Dugan <tadugan@me.com>
2021-09-08 10:52:50 -0500

    Completed review router

--- server/routes/review.router.js
+++ server/routes/review.router.js
@@ -9,9 +9,31 @@ const {
  * GET all STEMtells that have not been approved, filtered to only show stemtells for the user/teacher's classes
  */
 router.get('/', rejectUnauthenticated, (req, res) => {
-    const queryText = `
+    const teacherId = req.user.id;
     
+    const queryText = `
+    SELECT "user".name AS username, "user".id AS author_id, "stemtell".id AS stem_id, "stemtell".title, "stemtell".media_url, "stemtell".body_text, "user".profile_picture_url, "stemtell".unix, "class".name AS class_name
+    FROM "stemtell"
+    JOIN "user" ON "stemtell".user_id = "user".id
+    JOIN "class" ON "stemtell".class_id = "class".id
+    WHERE "stemtell".class_id IN (
+        SELECT class_id
+        FROM "user_class"
+        WHERE user_id = $1
+    ) 
+        AND "stemtell".approved = 'false'
+        AND "user".authority != 'teacher'
+    ;
     `;
+
+    pool.query(queryText, [teacherId])
+        .then(response => {
+            res.send(response.rows);
+        })
+        .catch(err => {
+            console.log("Error getting review STEMtells:", err);
+            res.sendStatus(500);
+          });
 
 });
 
